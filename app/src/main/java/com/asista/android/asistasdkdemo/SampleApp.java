2eb1108ed5f1b50459e882d326312531063c5425
Benjamin <cherry.assoc2@cherrylabsglobal.com>
2019-03-18 16:43:35 +0530

    -> asista SDK demo, functions include:   - login.   - registration.   - ticket creation.   - ticket list.   - Knowledge Base.   - Assets.

---
+++ app/src/main/java/com/asista/android/asistasdkdemo/SampleApp.java
@@ -0,0 +1,20 @@
+package com.asista.android.asistasdkdemo;
+
+import android.app.Application;
+
+/**
+ * <p>
+ * Created by Benjamin J on 02-11-2018.
+ * </p>
+ */
+public class SampleApp extends Application {
+    public static final String TAG = SampleApp.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+}
